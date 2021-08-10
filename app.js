3582a5be13eae8e02cb739010143b5b6e9f01693
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-10 19:28:32 +0800

    Changed html tags

--- app.js
+++ app.js
@@ -1,42 +1,63 @@
-var cashGiven=document.querySelector("#cashGiven");
-var billAmount=document.querySelector("#billAmount");
-var billAmountButton=document.querySelector("#billAmountButton");
-var cashGivenButton=document.querySelector("#cashGivenButton");
 
 
-var billReceived = 0;
-var cashReceived = 0;
-var totalChange= 0;
-var notes = [2000, 500, 100, 50, 10, 5, 2, 1];
 
- function billAmountClickHandler(){
-    billReceived= billAmount.value;
-    console.log(billReceived)
-}
 
-function cashGivenClickHandler(){
-    cashReceived = cashGiven.value;
-    console.log(cashReceived)
-}
-function calculateChange(){
-    totalChange= cashReceived - billReceived
-    console.log(totalChange)
-}
 
-function notesDivision(){
-    for (i=0; i < notes.length; i++){
-        if (totalChange > notes[i])
-        {
-            console.log(~~( totalChange /notes[i]) )
-        }
-        break;
 
-    }
 
-}
 
-billAmountButton.addEventListener('click', billAmountClickHandler);
-cashGivenButton.addEventListener("click", () =>{
-    cashGivenClickHandler();
-calculateChange();
-notesDivision()});
+
+
+
+
+
+
+
+
+
+
+
+
+//Below code logic was build by me then I was stucked so I watched the video and made above code without looking....
+// var cashGiven=document.querySelector("#cashGiven");
+// var billAmount=document.querySelector("#billAmount");
+// var billAmountButton=document.querySelector("#billAmountButton");
+// var cashGivenButton=document.querySelector("#cashGivenButton");
+
+
+// var billReceived = 0;
+// var cashReceived = 0;
+// var totalChange= 0;
+// var notes = [2000, 500, 100, 50, 10, 5, 2, 1];
+
+//  function billAmountClickHandler(){
+//     billReceived= billAmount.value;
+//     console.log(billReceived)
+// }
+
+// function cashGivenClickHandler(){
+//     cashReceived = cashGiven.value;
+//     console.log(cashReceived)
+// }
+// function calculateChange(){
+//     totalChange= cashReceived - billReceived
+//     console.log(totalChange)
+// }
+
+// function notesDivision(){
+//     for (i=0; i < notes.length; i++){
+//         if (totalChange > notes[i])
+//         {
+//             console.log( totalChange /notes[i] )
+//         }
+//         break;
+
+//     }
+
+// }
+
+// billAmountButton.addEventListener('click', billAmountClickHandler);
+// cashGivenButton.addEventListener("click", () =>{
+//     cashGivenClickHandler();
+// calculateChange();
+// notesDivision()});
