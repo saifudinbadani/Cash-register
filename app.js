43b269ef25411ed544f3dbedb0c71a9dd33841f6
Saifudin Badani <87968258+saifudinbadani@users.noreply.github.com>
2021-08-10 20:12:30 +0800

    making changecalculator func

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
+var billAmount = document.querySelector("#billAmount");
+var cashGiven= document.querySelector("#cashGiven");
+var checkButton = document.querySelector("#checkButton");
+var writeMessage=document.querySelector("#message");
+var noOfNotes=document.querySelectorAll("#noOfNotes");
+
+const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+writeMessage.style.display = 'none';
+
+checkButton.addEventListener('click', function validateBillAndCashGivenAmount(){
+    
+    if (billAmount.value > 0){
+        if (cashGiven.value >= billAmount.value){
+                calculateChange();
+        }
+        else {
+           showMessage ("Bhai free me le jao, tumse na ho payega!")
+        }
+
+    }
+    else {
+        
+        showMessage("Please enter amount greater than 0")
+    }
+})
 
 
+function calculateChange(){
+    for (i=0; i<notes.length; i++){
+     var changeToBeGiven = cashGiven.value - billAmount.value;
+     var numberOfNotes = Math.trunc(changeToBeGiven/notes[i])
+     changeToBeGiven = changeToBeGiven % notes[i];
+     noOfNotes[i].innerText= numberOfNotes
+    }
+    
+}
 
-
-
+function showMessage(message){
+    writeMessage.style.display = 'block';
+    writeMessage.innerText = message
+}
 
 
 
